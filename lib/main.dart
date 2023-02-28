1656eb137ffab0bd2985b1d0cab0716f5261f145
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-03-01 01:28:14 +0530

    (end of the day commit, scorrecord overflowing after final question still exists)

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,25 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   List<Icon> iconlist = [];
   late bool answer;
+  void Checkans(bool userpicked) {
+    answer = quizBrain.GetQuestionAns();
+    if (answer == userpicked) {
+      iconlist.add(
+        Icon(
+          Icons.close,
+          color: Colors.red,
+        ),
+      );
+    } else {
+      iconlist.add(
+        Icon(
+          Icons.check,
+          color: Colors.green,
+        ),
+      );
+    }
+    quizBrain.GetNextQuestion();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,101 +43,73 @@ class _MyAppState extends State<MyApp> {
       home: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.black,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(
-                child: Center(
-                  child: Text(
-                    quizBrain.GetQuestionText(),
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20, color: Colors.white),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      quizBrain.GetQuestionText(),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-                child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        answer = quizBrain.GetQuestionAns();
-                        if (answer == true) {
-                          iconlist.add(
-                            Icon(
-                              Icons.check,
-                              color: Colors.green,
-                            ),
-                          );
-                        } else {
-                          iconlist.add(
-                            Icon(
-                              Icons.close,
-                              color: Colors.red,
-                            ),
-                          );
-                        }
-                        quizBrain.GetNextQuestion();
-                      });
-                    },
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.green),
-                        foregroundColor:
-                            MaterialStateProperty.all(Colors.white)),
-                    child: const SizedBox(
-                      height: 60,
-                      child: Center(
-                          child: Text(
-                        "True",
-                        style: TextStyle(fontSize: 20),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                  child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          Checkans(true);
+                        });
+                      },
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.green),
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.white)),
+                      child: const SizedBox(
+                        height: 60,
+                        child: Center(
+                            child: Text(
+                          "True",
+                          style: TextStyle(fontSize: 20),
+                        )),
                       )),
-                    )),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-                child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        answer = quizBrain.GetQuestionAns();
-                        if (answer == true) {
-                          iconlist.add(
-                            Icon(
-                              Icons.close,
-                              color: Colors.red,
-                            ),
-                          );
-                        } else {
-                          iconlist.add(
-                            Icon(
-                              Icons.check,
-                              color: Colors.green,
-                            ),
-                          );
-                        }
-                        quizBrain.GetNextQuestion();
-                      });
-                    },
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.red),
-                        foregroundColor:
-                            MaterialStateProperty.all(Colors.white)),
-                    child: const SizedBox(
-                      height: 60,
-                      child: Center(
-                          child: Text(
-                        "False",
-                        style: TextStyle(fontSize: 20),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                  child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          Checkans(false);
+                        });
+                      },
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.red),
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.white)),
+                      child: const SizedBox(
+                        height: 60,
+                        child: Center(
+                            child: Text(
+                          "False",
+                          style: TextStyle(fontSize: 20),
+                        )),
                       )),
-                    )),
-              ),
-              Row(
-                children: iconlist,
-              ),
-              SizedBox(
-                height: 15,
-              )
-            ],
+                ),
+                Row(
+                  children: iconlist,
+                ),
+                SizedBox(
+                  height: 15,
+                )
+              ],
+            ),
           ),
         ),
       ),
